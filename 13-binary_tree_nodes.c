e55b6f57f22576e77d46d0efece5d0ab31ca9d62
MoeAlomar <Mohammedahalomar@gmail.com>
2025-04-10 18:28:15 +0000

    adding Task 14

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,25 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - it counts the nodes with at least 1 child
+ * binary_tree_nodes - it counts the nodes with at least one child
  * @tree: the root node
- * Return: it returns count of nodes with 1 child
+ * Return: it returns count of nodes with one child
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
+	/* return 0 if tree is null */
 	if (!tree)
 		return (0);
+
+	/* sets count 1 if there is at least one child */
 	if (tree->right || tree->left)
 		count = 1;
 
+	/* counts nodes with at least one child */
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
 
+	/* returns count of total nodes with one child */
 	return (count);
 }
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,57 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - it counts the height of the given node
+ * @tree: usually it is the root node
+ * Return: it returns the height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	/* attributes to calc left & right height */
+	size_t left = 0;
+	size_t right = 0;
+	
+	/* if tree == null then stop. */
+	if(!tree)
+		return (0);
+
+	/* checking if there is a left child therfore calculate it */
+	if (tree->left)
+	left = 1 + binary_tree_height(tree->left);
+
+	/* checking if there is a right child therfore calculate it */
+        if (tree->right)
+        right = 1 + binary_tree_height(tree->right);
+
+	if (left > right)
+		return (left);
+	
+	return (right);
+
+}
+/**
+ * binary_tree_balance - it calculates the difference of left & right
+ * @tree: the root node
+ * Return: returns the Balance formula
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height = 0
+	int right_height = 0;
+
+	/* if tree is null return 0; */
+	if (!tree)
+		return (0);
+	
+	/* calc left height */
+	if (tree->left)
+		left_height = binary_tree_height(tree->left);
+
+	/* calc right height */
+	if (tree->right)
+		right_height = binary_tree_height(tree->right);
+
+	/* Balance formula */
+	return (left_height - right_height);
+}
